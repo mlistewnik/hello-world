01e607cadb16dbed99c2abae823228e5d1d7e86c
Mariusz Listewnik <36151171+mlistewnik@users.noreply.github.com>
2020-09-06 13:29:38 +0200

    Update kalkulator VAT.py

--- kalkulator VAT.py
+++ kalkulator VAT.py
@@ -1,3 +1,4 @@
+#Oblicza cenę z VAT, możliwe podanie dowolnej stawki podatku
 cena_bez_vat = float (input ("Podaj cenę bez VAT: "))
 stawka_vat = float (input ("Podaj stawkę VAT: "))
 cena_koncowa = cena_bez_vat + cena_bez_vat*(stawka_vat/100)
