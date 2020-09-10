bf1dce4b59651586c33302b0acf77ff004cf02c2
Mariusz Listewnik <36151171+mlistewnik@users.noreply.github.com>
2020-09-10 08:06:08 +0200

    Update TrueAge.py

--- TrueAge.py
+++ TrueAge.py
@@ -1,8 +1,8 @@
 #Wyświetla "prawdziwy" wiek :D
 wiek =  int(input("Ile masz lat?\n"))
-if wiek == 18:
+if wiek <= 18:
     print("Jesteś szczylem!")
 elif wiek > 18 and wiek <=30:
-    print("Twój wiek to %d" % wiek)
+    print("Twój wiek to %d lat" % wiek)
 else:
     print("Masz 18 + VAT")
